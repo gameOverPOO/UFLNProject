f6df8f57d347b7f9c401326f8ab69e5bbae4975f
poney <poney@PoneyMint>
2015-05-06 21:07:59 -0300

    MenuPrincipal estruturado com switch case e entrada da opção pelo teclado.

--- sistemaDaUFLN/src/sistemadaufln/MenuPrincipal.java
+++ sistemaDaUFLN/src/sistemadaufln/MenuPrincipal.java
@@ -5,6 +5,9 @@
  */
 package sistemadaufln;
 
+
+import java.util.Scanner;
+
 /**
  *
  * @author Natália
@@ -15,7 +18,56 @@ public class MenuPrincipal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        Scanner leituraMenu = new Scanner(System.in);
+        int escolha=leituraMenu.nextInt();
+        
+        System.out.println("Lista de opcoes: ");
+        System.out.println("1. Cadastrar aluno");
+        System.out.println("2. Cadastrar professor");
+        System.out.println("3. Cadastrar disciplina");
+        System.out.println("4. Cadastrar turma");
+        System.out.println("5. Cadastrar atividade");
+        System.out.println("6. Lançar nota");
+        System.out.println("7. Lançar faltas");
+        
+        //fazer entrada scanf
+        switch(escolha){
+            case 1:{
+                System.out.println("entrar em AlunoView");
+                break;
+            }
+            case 2:{
+                System.out.println("entrar em ProfessorView");
+                break;
+            }
+            case 3:{
+                System.out.println("entrar em DisciplinaView");
+                break;
+            }
+            case 4:{
+                System.out.println("entrar em TurmaView");
+                break;
+            }
+            case 5:{
+                System.out.println("entrar em AtividadeView");
+                break;
+            }
+            case 6:{
+                System.out.println("entrar em NotaView");
+                break;
+            }
+            case 7:{
+                System.out.println("entrar em FaltaView");
+                break;
+            }
+            default:{
+                System.out.println("Opção Inválida");
+                break;
+            }
+        }
+        
     }
     
 }

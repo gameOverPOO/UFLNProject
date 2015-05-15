d5c325c1e2bccb43cdeeab7a8e1d608f09b70f74
LeandroNog <leandronog.ufes@gmail.com>
2015-05-15 10:06:48 -0300

    Update Arquivo.java

--- sistemaDaUFLN/src/sistemadaufln/Arquivo.java
+++ sistemaDaUFLN/src/sistemadaufln/Arquivo.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-
+/*SALVAR TODO O OBJETO...ALUNOS COM DISCIPLINAS, PROFESSOREES, ETC*/
 //Leia mais em: Criando e Gravando dados em Txt com Java http://www.devmedia.com.br/criando-e-gravando-dados-em-txt-com-java/23060#ixzz3ZmaRAJIh
 
 public class Arquivo {

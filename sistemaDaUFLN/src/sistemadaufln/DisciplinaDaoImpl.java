24bebf1853bb3ed60cb29f67b4311000ecded060
mari <mari@mari-HP-Pavilion-14-Notebook-PC>
2015-05-20 22:31:32 -0300

    implementacao dos metodos em DisciplinaDaoImpl

--- sistemaDaUFLN/src/sistemadaufln/DisciplinaDaoImpl.java
+++ sistemaDaUFLN/src/sistemadaufln/DisciplinaDaoImpl.java
@@ -6,23 +6,38 @@ import java.util.*;
 
 public class DisciplinaDaoImpl implements DisciplinaDao{
     
-    public List<Disciplina> disciplinal = new ArrayList<>(); 
+    public List<Disciplina> disciplinal = new ArrayList<Disciplina>(); 
    
     
     @Override
-    public void salvar(Object bean){
+    public void salvar(Disciplina disciplina){
         
+        disciplinal.add(disciplina);
+        System.out.println("size::: "+disciplinal.size());
     }
+
     @Override
-    public void atualizar(Object bean){
-        
+    public void atualizar(String nomeA, String nomeB){
+     
+        for(int i=0;i<disciplinal.size();i++)
+        {
+            Disciplina d=disciplinal.get(i);
+            if(d.getNome().equals(nomeB));
+        }
     }
     
-    @Override
-    public void deletar(int id){
+    
+    
+    @Override //mudar depois para ID
+    public void deletar(String nome){
+        
+        for(int i=0;i<disciplinal.size();i++){
+            
+            Disciplina d = disciplinal.get(i);
+            if(d.getNome().equals(nome))
+                disciplinal.remove(d);
+        }
         
     }
-    
-    
     
 }

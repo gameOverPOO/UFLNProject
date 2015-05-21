882d2005160a800d210991941c5b31c42109bd74
poney <poney@PoneyMint>
2015-05-21 15:29:19 -0300

    Restrição de casos adicionada

--- sistemaDaUFLN/src/sistemadaufln/MenuPrincipal.java
+++ sistemaDaUFLN/src/sistemadaufln/MenuPrincipal.java
@@ -14,54 +14,65 @@ public class MenuPrincipal {
         
         
         System.out.println("Lista de opcoes: ");
-        System.out.println("1. Cadastrar aluno");
-        System.out.println("2. Cadastrar professor");
-        System.out.println("3. Cadastrar disciplina");
-        System.out.println("4. Cadastrar turma");
-        System.out.println("5. Cadastrar atividade");
-        System.out.println("6. Lançar nota");
+        System.out.println("1. Aluno");
+        System.out.println("2. Professor");
+        System.out.println("3. Disciplina");
+        System.out.println("4. Turma");
+        System.out.println("5. Atividade");
+        System.out.println("6. Nota");
         System.out.println("7. Lançar faltas");
+        System.out.println("8. Sair");
         
         Scanner leituraMenu = new Scanner(System.in);
-        int escolha=leituraMenu.nextInt();
-        switch(escolha){
-            case 1:{
-                AlunoView alunoView = new AlunoView();
-                alunoView.subMenu();
-                break;
+        int escolha;
+        
+        do{
+            escolha=leituraMenu.nextInt();
+            switch(escolha){
+                case 1:{
+                    AlunoView alunoView = new AlunoView();
+                    alunoView.subMenu();
+                    break;
+                }
+                case 2:{
+                    ProfessorView professorView = new ProfessorView();
+                    professorView.subMenu();
+                    
+                    break;
+                }
+                case 3:{
+                    DisciplinaView disciplinaView = new DisciplinaView();
+                    disciplinaView.subMenu();                
+                    break;
+                }
+                case 4:{
+                    System.out.println("entrar em TurmaView");
+                    break;
+                }
+                case 5:{
+                    System.out.println("entrar em AtividadeView");
+                    break;
+                }
+                case 6:{
+                    System.out.println("entrar em NotaView");
+                    break;
+                }
+                case 7:{
+                    System.out.println("entrar em FaltaView");
+                    break;
+                }
+                case 8:{
+                    System.out.println("Saindo...");
+                    return;
+                }
+                default:{
+                    System.out.println("Opção Inválida");
+                    break;
+                }
             }
-            case 2:{
-                ProfessorView professorView = new ProfessorView();
-                professorView.subMenu();
-                
-                break;
-            }
-            case 3:{
-                DisciplinaView disciplinaView = new DisciplinaView();
-                disciplinaView.subMenu();                
-                break;
-            }
-            case 4:{
-                System.out.println("entrar em TurmaView");
-                break;
-            }
-            case 5:{
-                System.out.println("entrar em AtividadeView");
-                break;
-            }
-            case 6:{
-                System.out.println("entrar em NotaView");
-                break;
-            }
-            case 7:{
-                System.out.println("entrar em FaltaView");
-                break;
-            }
-            default:{
-                System.out.println("Opção Inválida");
-                break;
-            }
-        }
+            
+        }while(escolha<1 || escolha>8);
+        
         
     }
     

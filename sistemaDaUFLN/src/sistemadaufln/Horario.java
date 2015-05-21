62c15e83009d6dcf74972c27d6189095ee2d41a3
leandronog <leandronog@leandronog-G31T-M7>
2015-05-20 21:39:25 -0300

    Implementação de Métodos

--- sistemaDaUFLN/src/sistemadaufln/Horario.java
+++ sistemaDaUFLN/src/sistemadaufln/Horario.java
@@ -1,9 +1,26 @@
 
 package sistemadaufln;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.*;
+
 
 public class Horario {
     
+    private List<String> diaSemana; //
+    private final static String segunda = "Segunda-feira";
+    private final static String terca = "Terça-feira";
+    private final static String quarta = "Quarta-feira";
+    private final static String quinta = "Quinta-feira";
+    private final static String sexta = "Sexta-feira";
+    private final static String sabado = "Sábado";
+    private final static String domingo = "Domingo";
+    
+    
+    //Calendar calend = new GregorianCalendar();
+    
+    //a = calend.
     //Atributos
     //Horario de uma turma
     /*

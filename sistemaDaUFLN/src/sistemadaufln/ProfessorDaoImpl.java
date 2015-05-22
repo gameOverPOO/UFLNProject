b39785d50ecb8063ec8d7aa29dc850fe0d23f6f0
mari <mari@mari-HP-Pavilion-14-Notebook-PC>
2015-05-22 14:21:09 -0300

    atualizacao Professor

--- sistemaDaUFLN/src/sistemadaufln/ProfessorDaoImpl.java
+++ sistemaDaUFLN/src/sistemadaufln/ProfessorDaoImpl.java
@@ -5,7 +5,7 @@
  */
 package sistemadaufln;
 
-import java.util.List;
+import java.util.*;
 
 /**
  *
@@ -14,22 +14,47 @@ import java.util.List;
 public class ProfessorDaoImpl implements ProfessorDao{
     
     
+    public List<Professor> professorl = new ArrayList<>();
     
     //CRUD
     @Override
-    public void salvar(Object bean){
+    public void salvar(Professor professor){
+        
+        for(int i=0;i<professorl.size();i++){
+            Professor p = professorl.get(i);
+            if(p.getCpf().equals(professor.getCpf())){
+                System.out.println("Professor ja existente:");
+                return ;
+            }
+        }
+        
+        professorl.add(professor);
+        System.out.println("size::: "+professorl.size());
         
     }
     
+    
     @Override
-    public void atualizar(Object bean){
+    public void atualizar(String nomeA, String nomeN){
+        
+        for (Iterator<Professor> it = professorl.iterator(); it.hasNext();) {
+            Professor p = it.next();
+            if(p.getNome().equals(nomeA)) p.setNome(nomeN);
+        }       
+    }
+   
+    
+    @Override
+    public void deletar(String nome){
+        
+        for(int i = 0; i < professorl.size(); i++){
+          Professor p = professorl.get(i);
+          if(p.getNome().equals(nome)) 
+              professorl.remove(p);
+        }
         
     }
     
-    @Override
-    public void deletar(int id){
-        
-    }
     
     @Override
     public Object getById(int id){

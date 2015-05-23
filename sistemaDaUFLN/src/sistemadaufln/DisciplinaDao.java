a0f5d692d5bd93501b910ce68ff6a73845ed452d
mari <mari@mari-HP-Pavilion-14-Notebook-PC>
2015-05-23 20:19:25 -0300

    cadastros

--- sistemaDaUFLN/src/sistemadaufln/DisciplinaDao.java
+++ sistemaDaUFLN/src/sistemadaufln/DisciplinaDao.java
@@ -8,6 +8,9 @@ public interface DisciplinaDao {
     public void salvar(Disciplina disciplina);
     public void atualizar(String nomeA, String nomeB);
     public void deletar(String nome);
+    public void cadastrarProfessor(String cpf, String nomeD);
+    public void excluirProfessor(String cpf, String nomeD);
+    public void imprimirListaProfessor(String nomeD);
    
 
     

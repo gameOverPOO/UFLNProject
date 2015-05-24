e93dd03828c1ee5fa378291819831ac1cbf99471
mari <mari@mari-HP-Pavilion-14-Notebook-PC>
2015-05-24 18:47:21 -0300

    atualizacoes

--- sistemaDaUFLN/src/sistemadaufln/TurmaDao.java
+++ sistemaDaUFLN/src/sistemadaufln/TurmaDao.java
@@ -6,8 +6,14 @@ public interface TurmaDao {
     
    
     public void salvar(Turma aluno);
-    public void atualizar(String nomeA, String nomeN);
     public void deletar(Integer codigo);
     public void imprimirLista();
+    public void cadastrarAluno(String cpf, Integer codigo);
+    public void imprimirAlunos(Integer codigo);
+    public void cadastrarProfessor(String cpf, Integer codigo);
+    public void imprimirProfessores(Integer codigo);
+    public void atividade(String nome, Integer codigo);
+    public void imprimirAtividade(Integer codigo);
+    
     
 }

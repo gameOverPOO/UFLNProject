b06498aed6ec7708acb5e0a5fb635a62c0e79358
mari <mari@mari-HP-Pavilion-14-Notebook-PC>
2015-05-24 23:15:22 -0300

    Implementação Atividade

---
+++ sistemaDaUFLN/src/atividade/AtividadeDao.java
@@ -0,0 +1,17 @@
+
+package atividade;
+
+import atividade.Atividade;
+
+
+public interface AtividadeDao {
+    
+    public void salvar(Atividade atividade);
+    public void deletar(String nome);
+    public void cadastrarAluno(String cpf, String nomeA);
+    public void excluirAluno(String cpf, String nomeA);
+    public void imprimirAlunos(String nomeA);
+    
+}
+
+

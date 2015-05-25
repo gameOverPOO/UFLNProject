e0971c9c0c6ae52866f058d915c7912a4683d85f
LeandroNog <leandronog.ufes@gmail.com>
2015-05-24 23:56:44 -0300

    Update AtividadeDaoImpl.java

--- sistemaDaUFLN/src/atividade/AtividadeDaoImpl.java
+++ sistemaDaUFLN/src/atividade/AtividadeDaoImpl.java
@@ -25,7 +25,7 @@ public class AtividadeDaoImpl implements AtividadeDao{
             }    
         }
         atividadel.add(atividade);
-        System.out.println("size::: "+atividadel.size());
+       
         
         
     }

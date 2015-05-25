389bea5dd58c5a326adc393b213fee421482af23
mari <mari@mari-HP-Pavilion-14-Notebook-PC>
2015-05-24 23:36:35 -0300

    update

--- sistemaDaUFLN/src/turma/TurmaDao.java
+++ sistemaDaUFLN/src/turma/TurmaDao.java
@@ -13,7 +13,6 @@ public interface TurmaDao {
     public void cadastrarProfessor(String cpf, Integer codigo);
     public void imprimirProfessores(Integer codigo);
     public void atividade(String nome, Integer codigo);
-    public void imprimirAtividade(Integer codigo);
     
     
 }

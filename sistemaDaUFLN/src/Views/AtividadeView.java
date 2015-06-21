20bcc4dceba4d2f978920e50c0492178b4e1e811
poney <poney@PoneyMint>
2015-06-21 16:45:45 -0300

    ATIVIDADEVIEW: Cada case deveria ser uma chamada de método, não esse tanto de código. RESOLVIDO PARA ATIVIDADEVIEW

--- sistemaDaUFLN/src/Views/AtividadeView.java
+++ sistemaDaUFLN/src/Views/AtividadeView.java
@@ -24,128 +24,33 @@ public class AtividadeView {
             System.out.println("1. Adicionar uma Atividade");
             System.out.println("2. Remover uma Atividade");
             System.out.println("3. Cadastrar um Aluno em uma Atividade");
-            System.out.println("4. Excluir um Aluno de uma Atividade");
+            System.out.println("4. Remover um Aluno de uma Atividade");
             System.out.println("5. Imprimir lista de Aluno");
             System.out.println("6. Sair");
 
             int escolha = leituraMenu.nextInt();
             switch(escolha){
                 case 1:{
-                    System.out.println("Adicionando uma Atividade");
-
-
-                    int adicionar = 1;
-                    while(adicionar==1){
-
-                         Scanner leitura= new Scanner(System.in);
-                         System.out.println("Digite o nome da Atividade: ");
-
-                         String nome = new String();                
-                         nome = leitura.nextLine();
-
-
-                         System.out.println("Digite o tipo: ");
-                         String tipo = new String();                
-                         tipo = leitura.nextLine();
-
-                         System.out.println("Digite a Data (dd/MM/yyyy): ");
-                         String data = new String();
-                         data = leitura.nextLine();
-
-
-                         System.out.println("Digite o valor: ");
-
-                         Double valor = leitura.nextDouble();
-
-                         
-                         Atividade atividade = new Atividade(nome,tipo,data,valor);
-
-                         a.salvar(atividade);
-                         
-
-                         System.out.println("Deseja adicionar outra atividade? (S/N)");
-                         Scanner leituraNova= new Scanner(System.in);
-                         String opcao = new String();
-                         opcao = leituraNova.nextLine();                   
-
-                         if (opcao.equals("N")||opcao.equals("n")) 
-                             adicionar = 0;
-                         
-                    }
+                    while(adicionarAtividade()==1);
                     break;
                 }
                 case 2:{
-                     System.out.println("Digite o Nome da Atividade a ser deletado: ");
-                         Scanner leitura= new Scanner(System.in);
-                         String nome = new String();                
-                         nome = leitura.nextLine();
-                         
-                         Atividade atividaded = new Atividade();
-                        
-                         atividaded.setNome(nome);
-                         
-                         a.deletar(atividaded);
-
+                    removerAtividade();  
                     break;
                 }
                 case 3:
                 {
-                    System.out.println("Adicionando um aluno a atividade:");
-
-
-                    System.out.println("Digite o cpf do Aluno: ");
-                         Scanner leitura= new Scanner(System.in); 
-                         String cpf = new String();                
-                         cpf = leitura.nextLine();
-                         Aluno aluno = new Aluno();
-                         aluno.setCpf(cpf);
-
-                    System.out.println("Digite a atividade: ");
-                         String nome = new String();                
-                         nome = leitura.nextLine();
-                         
-                         
-                    Atividade atividade = new Atividade();
-                    atividade.setNome(nome);
-                    
-                    a.cadastrarAluno(aluno, atividade);
+                    adicionarAlunoAtividade();
                     break;
                 }
                 case 4:
                 {
-                    System.out.println("Excluindo Aluno:");
-
-
-                    System.out.println("Digite o cpf do Aluno: ");
-                         Scanner leitura= new Scanner(System.in); 
-                         String cpf = new String();                
-                         cpf = leitura.nextLine();
-                         Aluno aluno = new Aluno();
-                         aluno.setCpf(cpf);
-
-
-                    System.out.println("Digite o nome da atividade: ");
-                         String nome = new String();                
-                         nome = leitura.nextLine();
-                         Atividade atividade = new Atividade();
-                         atividade.setNome(nome);
-
-                    a.excluirAluno(aluno, atividade);
+                    removerAlunoAtividade();
                     break;
                 }
                 case 5:
                 {
-                    System.out.println("Imprimindo lista de alunos");
-
-                     Scanner leitura= new Scanner(System.in);    
-
-                    System.out.println("Digite o nome da atividade: ");
-                         String nome = new String();                
-                         nome = leitura.nextLine();
-                         Atividade atividade = new Atividade();
-                         atividade.setNome(nome);
-
-                    a.imprimirAlunos(atividade);
+                    imprimirAlunoAtividade();
                     break;
                 }
                 case 6:{
@@ -161,5 +66,104 @@ public class AtividadeView {
         }
     }
     
+    public int adicionarAtividade(){
+        System.out.println("Adicionando uma Atividade");                    
 
+        int adicionar = 1;
+                    
+
+        Scanner leitura= new Scanner(System.in);
+        System.out.println("Digite o nome da Atividade: ");
+        String nome = new String();                
+        nome = leitura.nextLine();
+
+        System.out.println("Digite o tipo: ");
+        String tipo = new String();                
+        tipo = leitura.nextLine();
+
+        System.out.println("Digite a Data (dd/MM/yyyy): ");
+        String data = new String();
+        data = leitura.nextLine();
+
+
+        System.out.println("Digite o valor: ");
+        Double valor = leitura.nextDouble();
+
+                         
+        Atividade atividade = new Atividade(nome,tipo,data,valor);
+
+        a.salvar(atividade);
+                         
+
+        System.out.println("Deseja adicionar outra atividade? (S/N)");
+        Scanner leituraNova= new Scanner(System.in);
+        String opcao = new String();
+        opcao = leituraNova.nextLine();                   
+
+        if (opcao.equals("N")||opcao.equals("n")) 
+            adicionar = 0;
+                         
+        return adicionar;
+    }
+    public void removerAtividade(){
+        System.out.println("Digite o Nome da Atividade a ser removido: ");
+        Scanner leitura= new Scanner(System.in);
+        String nome = new String();                
+        nome = leitura.nextLine();
+        Atividade atividaded = new Atividade();
+        atividaded.setNome(nome);                 
+        a.deletar(atividaded);
+
+    }
+    public void adicionarAlunoAtividade(){
+        System.out.println("Adicionando um aluno a atividade:");
+
+        System.out.println("Digite o cpf do Aluno: ");
+        Scanner leitura= new Scanner(System.in); 
+        String cpf = new String();                
+        cpf = leitura.nextLine();
+        Aluno aluno = new Aluno();
+        aluno.setCpf(cpf);
+
+        System.out.println("Digite a atividade: ");
+        String nome = new String();                
+        nome = leitura.nextLine();
+                         
+        Atividade atividade = new Atividade();
+        atividade.setNome(nome);
+                    
+        a.cadastrarAluno(aluno, atividade);
+    }
+    public void removerAlunoAtividade(){
+        System.out.println("Excluindo Aluno:");
+
+        System.out.println("Digite o cpf do Aluno: ");
+        Scanner leitura= new Scanner(System.in); 
+        String cpf = new String();                
+        cpf = leitura.nextLine();
+        Aluno aluno = new Aluno();
+        aluno.setCpf(cpf);
+
+        System.out.println("Digite o nome da atividade: ");
+        String nome = new String();                
+        nome = leitura.nextLine();
+        Atividade atividade = new Atividade();
+        atividade.setNome(nome);
+
+        a.excluirAluno(aluno, atividade);
+    }
+    public void imprimirAlunoAtividade(){
+        System.out.println("Imprimindo lista de alunos");
+
+        Scanner leitura= new Scanner(System.in);    
+
+        System.out.println("Digite o nome da atividade: ");
+        String nome = new String();                
+        nome = leitura.nextLine();
+        Atividade atividade = new Atividade();
+        atividade.setNome(nome);
+
+        a.imprimirAlunos(atividade);
+    }
+    
 }
